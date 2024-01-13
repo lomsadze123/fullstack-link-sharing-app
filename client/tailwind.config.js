6ee70f9156d86647970417909ee8975a7a163a1c
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-01-13 18:48:57 +0400

    add fonts and tailwind tools

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#633CFF",
+        purpleMedium: "#BEADFF",
+        purpleLight: "#EFEBFF",
+        blackMedium: "#333333",
+        blackLight: "#737373",
+        greyLight: "#D9D9D9",
+        whiteBold: "#FAFAFA",
+        red: "#FF3939",
+      },
+    },
   },
   plugins: [],
 };
