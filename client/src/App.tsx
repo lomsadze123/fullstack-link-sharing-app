92a11ebbb5ab5a119cfe27e5ba7a2bd9525cb7ba
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-02-09 12:25:17 +0400

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,16 @@ import Header from "./components/header/Header";
 const App = () => {
   const location = useLocation();
 
+  const image =
+    "https://github.com/lomsadze123/audiophile-ecommerce-website/blob/master/src/assets/home/desktop/image-hero.jpg?raw=true";
+
   return (
     <div className="">
+      <img
+        src="https://github.com/lomsadze123/audiophile-ecommerce-website/blob/master/src/assets/home/desktop/image-hero.jpg?raw=true"
+        alt=""
+      />
+      <img src={image} alt="" />
       {location.pathname !== "/" && <Header />}
       <Routes>
         <Route
