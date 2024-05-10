1a642d792f1879c663f37cb9be75bab25ac1231c
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-05-10 20:11:01 +0400

    remove server side due to firebase

--- src/components/saveButton/SaveButton.tsx
+++ src/components/saveButton/SaveButton.tsx
@@ -1,10 +1,10 @@
 const SaveButton = ({ number }: { number?: number[] }) => {
   return (
-    <div className="bg-white py-5 border-t border-t-grey-500 fixed bottom-0 left-4 w-[90%]">
+    <div className="bg-white py-5 border-t border-t-grey-500 fixed bottom-0 left-4 w-[90%] md:static md:w-full md:text-right">
       <button
         className={`w-full py-2 rounded-lg ${
           number?.length === 0 && "bg-opacity-50"
-        } bg-purple text-white font-semibold`}
+        } bg-purple text-white font-semibold md:w-auto md:px-7`}
       >
         Save
       </button>
